03947427f38f3d46556ff9c8a668332809a8cf32
Shu Maeda <shu2022shu@docomo.ne.jp>
2024-10-16 10:53:10 +0900

    Update arai60_1_step3.py

--- arai60_1_step3.py
+++ arai60_1_step3.py
@@ -1,14 +1,14 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hashset = set() # set of nodes pointer already visited
+        hashset = set()
         current = head
 
-        while current != None:
+        while current:
             if current in hashset:
                 return True
             hashset.add(current)
             current = current.next
         return False
-
 #時間計算量はO(n) 空間計算量はO(n)
+#Runtime: 45ms  Memory: 19.67MB
 
