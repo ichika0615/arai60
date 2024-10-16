6fc3dd46bdba703b488458c969bf0ab399ac9ddf
Shu Maeda <shu2022shu@docomo.ne.jp>
2024-10-16 11:07:12 +0900

    Update arai60_1_step3.py
    new commit

--- arai60_1_step3.py
+++ arai60_1_step3.py
@@ -1,3 +1,4 @@
+Python
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         hashset = set()
@@ -9,6 +10,22 @@ class Solution:
             hashset.add(current)
             current = current.next
         return False
+
 #時間計算量はO(n) 空間計算量はO(n)
 #Runtime: 45ms  Memory: 19.67MB
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+#時間計算量O(n)  空間計算量O(1)
+#Runtime: 35ms  Memory: 19.12MB (思ったよりメモリー変わらない)
+
 
